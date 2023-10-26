d3ca10a25862de26275b542804dfa99b0582826e
Steeve Trincal <steeve.trincal@gmail.com>
2023-10-26 18:29:59 +0200

    fixed a bug on login

--- connexion.php
+++ connexion.php
@@ -1,25 +1,24 @@
 <?php
 include 'config.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pseudo']) && isset($_POST['mot_de_passe'])) {
+if (($_SERVER['REQUEST_METHOD'] === 'POST') && (isset($_POST['pseudo'])) && (isset($_POST['pass']))) {
     $pseudo = $_POST['pseudo'];
-    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); // Hasher le mot de passe
+    $pass = $_POST['pass'];
 
-    // Vérifier si l'email existe déjà
-    $sql = "SELECT id FROM users WHERE pseudo = ?";
+    $sql = "SELECT * FROM users WHERE pseudo = ?";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$pseudo]);
+    $user = $stmt->fetch();
 
-    if ($stmt->rowCount() > 0) {
-        echo "L'utilisateur avec ce pseudo existe déjà. Veuillez choisir un autre pseudo.";
-        echo '<script>alert("Ce pseudo est déjà pris. Veuillez en choisir un autre.");</script>';
+    if ($user && password_verify($pass, $user['pass'])) {
+        // Connexion réussie
+        echo "Connexion réussie. Bienvenue, " . $user['pseudo'] . "!";
+        echo '<script>alert("Connexion réussie !");</script>';
+        header('Location: espacemembre.html');
+        exit;
     } else {
-        // Insérer le nouvel utilisateur
-        $sql = "INSERT INTO users (pseudo, pass) VALUES (?, ?)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$pseudo, $mot_de_passe]);
-        echo "Inscription réussie. Bienvenue, " . $pseudo . "!";
-        echo '<script>alert("Inscription réussie !");</script>';
+        echo "Identifiants incorrects. Veuillez réessayer.";
+        echo '<script>alert("Login Faux !");</script>';
     }
 }
 ?>
