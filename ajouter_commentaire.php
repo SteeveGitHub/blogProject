b4cee5b9a23ea0c536fe740c4b0f416d95feca54
Dyllan Thouvignon <dthouvignon@myges.fr>
2023-11-30 16:27:55 +0100

    commit

---
+++ ajouter_commentaire.php
@@ -0,0 +1,25 @@
+<?php
+include 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['commentaire']) && isset($_POST['articleId'])) {
+    $commentaire = $_POST['commentaire'];
+    $articleId = $_POST['articleId'];
+
+    session_start();
+
+    if (isset($_SESSION['userId'])) {
+        $userId = $_SESSION['userId'];
+
+        $insertSql = "INSERT INTO commentaires (idarticle, iduser, contenu) VALUES (?, ?, ?)";
+        $insertStmt = $pdo->prepare($insertSql);
+
+        if ($insertStmt->execute([$articleId, $userId, $commentaire])) {
+            echo "Commentaire ajouté avec succès!";
+        } else {
+            echo "Erreur lors de l'ajout du commentaire.";
+        }
+    } else {
+        echo "Veuillez vous connecter pour ajouter un commentaire.";
+    }
+}
+?>
